cba7cb9b0af79035acc080bf1d5bc990f16801f8
jorge <jorgelopezgonzalez474@gmail.com>
2020-09-22 14:56:15 -0300

    Added Sidebar

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const LoginScreen = () => {
   return (
-    <div>
+    <>
       <h1 className="auth__title">Login</h1>
 
       <form>
@@ -44,6 +44,6 @@ export const LoginScreen = () => {
           Create new account
         </Link>
       </form>
-    </div>
+    </>
   );
 };
--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,9 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const RegisterScreen = () => {
   return (
-    <div>
-      <h1>RegisterScreen</h1>
-    </div>
+    <>
+      <h1 className="auth__title">Register</h1>
+
+      <form>
+        <input
+          type="text"
+          autoComplete="off"
+          placeholder="Name"
+          name="name"
+          className="auth__input"
+        />
+        <input
+          type="text"
+          autoComplete="off"
+          placeholder="Email"
+          name="email"
+          className="auth__input"
+        />
+        <input
+          type="password"
+          autoComplete="off"
+          placeholder="Password"
+          name="password"
+          className="auth__input"
+        />
+        <input
+          type="password"
+          autoComplete="off"
+          placeholder="Confirm password"
+          name="password2"
+          className="auth__input"
+        />
+        <button className="btn btn-primary btn-block mb-5" type="submit">
+          login
+        </button>
+
+        <Link className="link " to="/auth/login">
+          Already registered?
+        </Link>
+      </form>
+    </>
   );
 };
