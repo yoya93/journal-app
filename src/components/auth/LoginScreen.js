b2de8d9edd6c5fc5bac646e554deb84623aa5580
jorge <jorgelopezgonzalez474@gmail.com>
2020-09-22 10:01:24 -0300

    Added styles in LoginScreen

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const LoginScreen = () => {
   return (
     <div>
-      <h1>LoginScreen</h1>
+      <h1 className="auth__title">Login</h1>
 
       <form>
         <input
@@ -11,18 +12,34 @@ export const LoginScreen = () => {
           autoComplete="off"
           placeholder="email"
           name="email"
+          className="auth__input"
         />
         <input
           type="password"
           autoComplete="off"
           placeholder="password"
           name="password"
+          className="auth__input"
         />
         <button type="submit">login</button>
         <hr />
-        <div>
+        <div className="auth__social-networks">
           <p>Login with social networks</p>
+          <div className="google-btn">
+            <div className="google-icon-wrapper">
+              <img
+                className="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="google button"
+              />
+            </div>
+            <p className="btn-text">
+              <b>Sign in with google</b>
+            </p>
+          </div>
         </div>
+
+        <Link to="/auth/register">Create new account</Link>
       </form>
     </div>
   );
