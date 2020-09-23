bfa0df0b06d1d9d7e2c2d41220802d67a29140ca
jorge <jorgelopezgonzalez474@gmail.com>
2020-09-23 18:40:52 -0300

    Added validation register component

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import validator from "validator";
 
 import { useForm } from "../../hooks/useForm";
 
@@ -15,7 +16,28 @@ export const RegisterScreen = () => {
 
   const handleRegister = (e) => {
     e.preventDefault();
+    if (isFormValid()) {
+      console.log("Formulario correcto");
+    }
+
     console.log(name);
+  };
+
+  const isFormValid = () => {
+    if (name.trim().length === 0) {
+      console.log("name is required");
+      return false;
+    } else if (!validator.isEmail(email)) {
+      console.log("Email is not valid");
+      return false;
+    } else if (password !== password2 || password.length < 5) {
+      console.log(
+        "Password should be at least 6 characters and match each other"
+      );
+      return false;
+    }
+
+    return true;
   };
 
   return (
