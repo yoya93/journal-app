77ed58c2cc0c6aa0d5ba4bbe9873e0ba0983df6f
jorge <jorgelopezgonzalez474@gmail.com>
2020-09-28 22:52:54 -0300

    Added Linear progres from material-ui

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,28 +1,39 @@
-import React, { useEffect } from "react";
-
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
-import { AuthRouter } from "./AuthRouter";
-import { JournalScreen } from "../components/journal/JournalScreen";
 import { useDispatch } from "react-redux";
-import { login } from "../components/actions/auth";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
+import { JournalScreen } from "../components/journal/JournalScreen";
+import { login } from "../components/actions/auth";
+import { AuthRouter } from "./AuthRouter";
 import { firebase } from "../firebase/firebase-config";
 
 export const AppRouter = () => {
+  const [checking, setChecking] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user?.uid) {
         dispatch(login(user.uid, user.displayName));
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
       }
+
+      setChecking(false);
     });
-  }, [dispatch]);
+  }, [dispatch, setChecking, setIsLoggedIn]);
+
+  if (checking) {
+    return <LinearProgress />;
+  }
 
   return (
     <Router>
