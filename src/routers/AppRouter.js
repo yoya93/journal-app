9b7fe62757c9e4182b770bd07a41bd6ffd1df485
jorge <jorgelopezgonzalez474@gmail.com>
2020-09-28 22:31:04 -0300

    hold info after refresh pag

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import {
   BrowserRouter as Router,
@@ -8,8 +8,22 @@ import {
 } from "react-router-dom";
 import { AuthRouter } from "./AuthRouter";
 import { JournalScreen } from "../components/journal/JournalScreen";
+import { useDispatch } from "react-redux";
+import { login } from "../components/actions/auth";
+
+import { firebase } from "../firebase/firebase-config";
 
 export const AppRouter = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName));
+      }
+    });
+  }, [dispatch]);
+
   return (
     <Router>
       <div>
