81afc88e3b33435b601dfafcde2a754c7e73ef16
jorge <jorgelopezgonzalez474@gmail.com>
2020-10-03 20:02:21 -0300

    test in action startSaveNotes

--- src/tests/components/actions/notes.test.js
+++ src/tests/components/actions/notes.test.js
@@ -6,6 +6,8 @@ import {
   startNewNotes,
   deleteNote,
   startDeleting,
+  startLoadingNotes,
+  startSaveNotes,
 } from "../../../components/actions/notes";
 import { types } from "../../../types/types";
 import { db } from "../../../firebase/firebase-config";
@@ -14,13 +16,19 @@ const middlewares = [thunk];
 
 const mockStore = configureStore(middlewares);
 
-const store = mockStore({
+const initState = {
   auth: {
     uid: "TESTING",
   },
-});
+};
+
+let store = mockStore(initState);
 
 describe("pruebas en las aciones notes (asincronas)", () => {
+  beforeEach(() => {
+    store = mockStore(initState);
+  }); // limpio el store para que se borren las acciones antes de cada prueba
+
   test("debe de crear una nueva nota startNote", async () => {
     await store.dispatch(startNewNotes());
 
@@ -57,5 +65,48 @@ describe("pruebas en las aciones notes (asincronas)", () => {
     // });
 
     // console.log(actions);
+  }, 20000);
+
+  test("debe de cargar las  notas startLoadingNotes", async () => {
+    await store.dispatch(startLoadingNotes(initState.auth));
+
+    const actions = store.getActions();
+    const expected = {
+      id: expect.any(String),
+      title: expect.any(String),
+      body: expect.any(String),
+      date: expect.any(Number),
+    };
+    expect(actions[0].payload[0]).toMatchObject(expected);
+
+    expect(actions[0]).toEqual({
+      type: types.notesLoad,
+      payload: expect.any(Array),
+    });
+  }, 20000);
+
+  test("startSaveNotes debe de actualizar la nota", async () => {
+    const note = {
+      id: "nMptx4590bMtamI0Fl6v",
+      title: "Mi titulo de test",
+      body: "body Test",
+    };
+
+    await store.dispatch(startSaveNotes(note));
+
+    const actions = store.getActions();
+
+    const noteCloud = await db.doc(`TESTING/journal/notes/${note.id}`).get();
+
+    expect(actions[0].type).toBe(types.notesUpdate);
+    expect(note.title).toEqual(noteCloud.data().title);
+
+    // const expected = {
+    //   id: expect.any(String),
+    //   title: expect.any(String),
+    //   body: expect.any(String),
+    //   date: expect.any(Number),
+    // };
+    // expect(actions[0].payload[0]).toMatchObject(expected);
   }, 20000);
 });
