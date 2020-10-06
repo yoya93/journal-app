7e0ed0e50848fc45abe0f0152682e4cb2bb4e189
jorge <jorgelopezgonzalez474@gmail.com>
2020-10-05 22:48:07 -0300

    added docs builds

---
+++ docs/precache-manifest.44423c3a7df6860fede6e02bef4a5317.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0b5e17c903e4d74367fde1f904fe65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c29c174d423a095c7e6",
+    "url": "/static/css/main.c0605716.chunk.css"
+  },
+  {
+    "revision": "721d2fc35f8a5cdcb48e",
+    "url": "/static/js/2.5589a91d.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.5589a91d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c29c174d423a095c7e6",
+    "url": "/static/js/main.b0e100c2.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
